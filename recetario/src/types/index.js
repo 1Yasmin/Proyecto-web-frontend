469b43df4f0bdb80ae4b8a0c28c98aa93d11cf0f
Cpzty <Cristianpzty@gmail.com>
2018-10-24 15:01:48 -0600

    Users

--- recetario/src/types/index.js
+++ recetario/src/types/index.js
@@ -1,3 +1,7 @@
 export const ADD_RECIPE = 'ADD_RECIPE';
 export const DELETE_RECIPE = 'DELETE_RECIPE';
 export const MODIFY_RECIPE = 'MODIFY_RECIPE';
+
+export const USER_ADDED = 'USER_ADDED';
+export const USER_REMOVED = 'USER_REMOVED';
+export const USER_CONFIRMED = 'USER_CONFIRMED';
