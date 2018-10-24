6b6942f524b02dd0e689c9992bc9b939dece7ad0
Cpzty <Cristianpzty@gmail.com>
2018-10-24 15:12:11 -0600

    reducer de user

--- recetario/src/actions/index.js
+++ recetario/src/actions/index.js
@@ -1,5 +1,26 @@
 import * as types from '../types';
 
+export const addUser = (id, firstName, lastName, email, username) => ({
+  type: types.USER_ADDED,
+  payload: {
+    id,
+    firstName,
+    lastName,
+    email,
+    username,
+  },
+});
+
+export const removeUser = id => ({
+  type: types.USER_REMOVED,
+  payload: { id },
+});
+
+export const confirmUser = id => ({
+  type: types.USER_CONFIRMED,
+  payload: { id },
+});
+
 export const addRecipe = (titulo,descripcion)=>({
     type:types.ADD_RECIPE,
     payload:{
