54ac12483bfd8faf674189a93049a1aa953092d4
Cpzty <Cristianpzty@gmail.com>
2018-10-31 14:27:57 -0600

    middleware a store

--- recetario/src/configstore/store.js
+++ recetario/src/configstore/store.js
@@ -1,8 +1,15 @@
-import { createStore } from 'redux';
-
+import { createStore, applyMiddleware, compose  } from 'redux';
+import createSagaMiddleware from 'redux-saga';
 //import root reducer
 import Rootreducer from '../reducers/index';
+//import sagas
+//import Saga from './Sagas';
+const sagaMiddleware = createSagaMiddleware();
 
-const store = createStore(Rootreducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const middleware = applyMiddleware(sagaMiddleware);
+
+const store = createStore(Rootreducer,compose(middleware, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
+
+//sagaMiddleware.run(Saga);
 
 export default store;
