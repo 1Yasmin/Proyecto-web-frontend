c33c0bffe2dc07b2778da4ac57f0c1ad758f5745
Cpzty <Cristianpzty@gmail.com>
2018-11-03 18:54:54 -0600

    register implementado con redux thunk

--- recetario/src/configstore/store.js
+++ recetario/src/configstore/store.js
@@ -2,11 +2,13 @@ import { createStore, applyMiddleware, compose  } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 //import root reducer
 import Rootreducer from '../reducers/index';
+
+import thunk from "redux-thunk";
 //import sagas
 //import Saga from './Sagas';
 const sagaMiddleware = createSagaMiddleware();
 
-const middleware = applyMiddleware(sagaMiddleware);
+const middleware = applyMiddleware(thunk);
 
 const store = createStore(Rootreducer,compose(middleware, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
 
