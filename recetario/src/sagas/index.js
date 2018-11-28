40aca925305b4ec5d593dc8d29bd47b0d3796f31
Yasmin Chavez <ychavez2014ig@gmail.com>
2018-11-28 15:53:18 -0600

    crear receta

--- recetario/src/sagas/index.js
+++ recetario/src/sagas/index.js
@@ -1,12 +1,13 @@
 import { takeEvery } from 'redux-saga/effects';
 
 import * as types from '../types';
-import { postUser, login } from './users'; 
+import { postUser, login, crearReceta } from './users'; 
 
 
 function* Saga() {
     yield takeEvery(types.USER_ADDED, postUser); // crear usuarios
     yield takeEvery(types.LOGIN_STARTED, login); // crear login
+    yield takeEvery(types.ADD_RECIPE, crearReceta); // crear login
 }
 
 export default Saga;
